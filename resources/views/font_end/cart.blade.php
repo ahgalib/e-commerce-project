8b1048ab2e6e45091373d0a1faff933e5f8a0feb
user name <asadullahalgalib783@gmail.com>
2022-04-23 15:58:49 +0600

    solve some issue

--- resources/views/font_end/cart.blade.php
+++ resources/views/font_end/cart.blade.php
@@ -88,20 +88,19 @@
 
     <table class="table table-bordered">
         <tbody>
-                <tr>
-                <td> 
-            <form class="form-horizontal">
-            <div class="control-group">
-            <label class="control-label"><strong> VOUCHERS CODE: </strong> </label>
-            <div class="controls">
-            <input type="text" class="input-medium" placeholder="CODE">
-            <button type="submit" class="btn"> ADD </button>
-            </div>
-            </div>
-            </form>
-            </td>
+            <tr>
+            <td> 
+				<form class="form-horizontal">
+				<div class="control-group">
+				<label class="control-label"><strong> VOUCHERS CODE: </strong> </label>
+				<div class="controls">
+				<input type="text" class="input-medium" placeholder="CODE">
+				<button type="submit" class="btn"> ADD </button>
+				</div>
+				</div>
+				</form>
+				</td>
             </tr>
-            
         </tbody>
     </table>
 			
